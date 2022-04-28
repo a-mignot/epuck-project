4a2defa01eff5690dc80367411e6d14686f5e0d8
Antonin Mignot <antonin.mignot@epfl.ch>
2022-04-28 09:58:02 +0200

    Repair main.h

--- main.h
+++ main.h
@@ -12,11 +12,13 @@ extern "C" {
 
 //--------- INCLUDES ---------
 
+#include "msgbus/messagebus.h"
+#include "parameter/parameter.h"
 
 /** Robot wide IPC bus. */
-//extern messagebus_t bus;
-//
-//extern parameter_namespace_t parameter_root;
+extern messagebus_t bus;
+
+extern parameter_namespace_t parameter_root;
 
 #ifdef __cplusplus
 }
