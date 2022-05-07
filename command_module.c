245fdad610bae0d1ff5256ebcb74065fd141be93
David Fontes Junqueira <david.fontesjunqueira@epfl.ch>
2022-05-07 10:35:53 +0200

    Added empty command thread

---
+++ command_module.c
@@ -0,0 +1,61 @@
+//Filename 		: command_module.c
+//Description  	: This module contains functions that define the ePuck response
+//to the values measured by sensors (peut être trop vague)
+//Authors 		: Antonin Mignot, David Junqueira
+//Version	 	: 1
+
+
+//--------- INCLUDES --------
+#include<command_module.h>
+#include<motors.h>
+
+
+//--------- DEFINES ---------
+
+
+//--------- FUNCTIONS ---------
+
+
+
+//main thread qui crée les threads de séquence
+static THD_FUNCTION(command_thd, arg){
+
+	(void) arg;
+	chRegSetThreadName(__FUNCTION__);
+
+
+	for(;;){
+	}
+
+}
+
+
+
+
+void command_start(){
+	 static THD_WORKING_AREA(command_thd_wa, 1024); // taille à fix en fonction de ce que j'ajoute comme variables
+	 chThdCreateStatic(command_thd_wa, sizeof(command_thd_wa), NORMALPRIO, command_thd, NULL);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ command_module.h
@@ -0,0 +1,23 @@
+//Filename 		: command_module.h
+//Description  	: Header of command_module
+//Authors 		: Antonin Mignot, David Junqueira
+//Version	 	: 1
+
+
+#ifndef COMMAND_MODULE_H
+#define COMMAND_MODULE_H
+
+//--------- INCLUDES --------
+
+
+//--------- DEFINES ---------
+
+
+
+//--------- PROTOTYPES ---------
+
+void command_start(void);
+
+#endif
+
+//--------- END OF FILE ---------
